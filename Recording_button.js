dc81557bd3557b407b5d666309808db050beaeb0
Xevilopez <72654044+Xevilopez@users.noreply.github.com>
2023-01-24 00:37:43 +0100

    Update Recording_button.js

--- Recording_button.js
+++ Recording_button.js
@@ -15,20 +15,8 @@ recordButton.addEventListener('click', () => {
     mediaRecorder.ondataavailable = e => {
       const recordedBlob = e.data;
       // Save the recorded video file
-        // Create a new MediaStreamRecording object
-            const mediaStreamRecording = new MediaStreamRecording(stream);
-                // Use the saveToFile method to save the video to a file
-                mediaStreamRecording.saveToFile('recorded-video.mp4'); /*.then(file => {
-                  // Use the PHPhotoLibrary class to save the video to the Photos app
-                  const photoLibrary = new photoLibrary();
-                  photoLibrary.writeImageDataToSavedPhotosAlbum(file, 'video/mp4', (error) => {
-                      if (error) {
-                          console.error('Error saving video to Photos app:', error);
-                      } else {
-                          console.log('Video saved successfully to Photos app');
-                      }
-                  });
-              });*/
+        const mediaStreamRecording = new MediaStreamRecording(stream);
+        mediaStreamRecording.saveToFile('recorded-video.mp4');
           };  
       }
     )
