2c79f192f2015890f7337263ad6818ac8e8b552a
Xevilopez <72654044+Xevilopez@users.noreply.github.com>
2023-01-24 17:27:08 +0100

    Update Recording_button.js

--- Recording_button.js
+++ Recording_button.js
@@ -2,24 +2,31 @@ const recordButton = document.getElementById('record-button');
 recordButton.addEventListener('click', () => {
     navigator.mediaDevices.getUserMedia({
         audio: true,
-        video: { facingMode: 'environment' }
+        video: true
     })
     .then(stream => {
-      // Start recording
-      const mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.start();
-      recordButton.addEventListener('click', () => {
-        // Stop the recording
-        mediaRecorder.stop();
-    });
-    mediaRecorder.ondataavailable = e => {
-      const recordedBlob = e.data;
-      // Save the recorded video file
-        const mediaStreamRecording = new MediaStreamRecording(stream);
-        mediaStreamRecording.saveToFile('recorded-video.mp4');
-          };  
-      }
-    )
+        // code to start recording and saving the video file goes here
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+
+        recordButton.addEventListener('click', () => {
+          // Stop the recording
+          mediaRecorder.stop();
+
+          mediaRecorder.ondataavailable = e => {
+            const recordedBlob = e.data;
+            // code to save the recorded video file goes here
+            const url = URL.createObjectURL(recordedBlob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'recorded-video.mp4';
+            document.body.appendChild(a);
+            a.click();
+        }
+
+      });
+    })
     .catch(error => {
         console.error('Error:', error);
     });
